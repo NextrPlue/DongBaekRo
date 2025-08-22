cf670fbb44557ff71f4f566012750d4c0f701bec
NextrPlue <98487008+NextrPlue@users.noreply.github.com>
2025-08-22 23:55:27 +0900

    feat: 프로젝트 초기 설정 (#1)
    - gradle 기반 백엔드 설정

---
+++ backend/src/main/java/com/redstonetorch/dongbaekro/DongBaekRoApplication.java
@@ -0,0 +1,13 @@
+package com.redstonetorch.dongbaekro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DongBaekRoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DongBaekRoApplication.class, args);
+	}
+
+}
