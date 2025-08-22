5c66416105852a0079fc9d3e8f10ae9975f569fc
NextrPlue <98487008+NextrPlue@users.noreply.github.com>
2025-08-23 01:23:42 +0900

    feat: 안전 시설 entity 및 repository 추가 (#7)
    * feat: 안전 시설 enum 타입 추가
    
    * feat: 안전 시설 entity 추가
    
    * refactor: 복수형 -> 단수형으로 이름 변경
    
    * feat: 안전 시설 repository 추가

---
+++ backend/src/main/java/com/redstonetorch/dongbaekro/safetyFacility/entity/SafetyFacility.java
@@ -0,0 +1,72 @@
+package com.redstonetorch.dongbaekro.safetyFacility.entity;
+
+import java.time.LocalDateTime;
+
+import com.redstonetorch.dongbaekro.common.enums.SafetyFacilityType;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "safety_facilities")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class SafetyFacility {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	@Enumerated(EnumType.STRING)
+	private SafetyFacilityType type;
+
+	@Column(nullable = false)
+	private String name;
+
+	@Column(nullable = false)
+	private Double latitude;
+
+	@Column(nullable = false)
+	private Double longitude;
+
+	@Column(nullable = false)
+	private String address;
+
+	@Column(nullable = false)
+	private LocalDateTime createdAt;
+
+	@Column(nullable = false)
+	private LocalDateTime updatedAt;
+
+	@PrePersist
+	protected void onCreate() {
+		createdAt = LocalDateTime.now();
+		updatedAt = LocalDateTime.now();
+	}
+
+	@PreUpdate
+	protected void onUpdate() {
+		updatedAt = LocalDateTime.now();
+	}
+
+	@Builder
+	public SafetyFacility(SafetyFacilityType type, String name, Double latitude, Double longitude, String address) {
+		this.type = type;
+		this.name = name;
+		this.latitude = latitude;
+		this.longitude = longitude;
+		this.address = address;
+	}
+}
---
+++ backend/src/main/java/com/redstonetorch/dongbaekro/safetyFacility/repository/SafetyFacilityRepository.java
@@ -0,0 +1,7 @@
+package com.redstonetorch.dongbaekro.safetyFacility.repository;
+
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(exported = false)
+public interface SafetyFacilityRepository {
+}
