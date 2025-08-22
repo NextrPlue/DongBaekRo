b7768f3422bd2d7c695c464be017f1a33d71a190
NextrPlue <98487008+NextrPlue@users.noreply.github.com>
2025-08-22 23:55:27 +0900

    feat: 프로젝트 초기 설정 (#1)
    - gradle 기반 백엔드 설정

---
+++ backend/src/test/java/com/redstonetorch/dongbaekro/DongBaekRoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redstonetorch.dongbaekro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DongBaekRoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
