46f005b4f4cea2681f7d6373364ff0445d8fb7dc
NexTR_Plue <p.plue1881@gmail.com>
2025-08-23 18:17:53 +0900

    feat: code로 table 조회하는 로직 추가

--- backend/src/main/java/com/redstonetorch/dongbaekro/location/repository/SafetyFacilityRepository.java
+++ backend/src/main/java/com/redstonetorch/dongbaekro/location/repository/SafetyFacilityRepository.java
@@ -1,7 +1,13 @@
 package com.redstonetorch.dongbaekro.location.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import com.redstonetorch.dongbaekro.location.entity.SafetyFacility;
+
 @RepositoryRestResource(exported = false)
-public interface SafetyFacilityRepository {
+public interface SafetyFacilityRepository extends JpaRepository<SafetyFacility, Long> {
+	List<SafetyFacility> findByCode(String code);
 }
