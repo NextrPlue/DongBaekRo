6dcac595b4986b69a7934ac95cd72821811ba21c
NexTR_Plue <p.plue1881@gmail.com>
2025-08-23 15:50:40 +0900

    feat: 긴급 신고 repository RepositoryRestResource 어노테이션 추가

--- backend/src/main/java/com/redstonetorch/dongbaekro/emergencyReport/repository/EmergencyReportRepository.java
+++ backend/src/main/java/com/redstonetorch/dongbaekro/emergencyReport/repository/EmergencyReportRepository.java
@@ -1,8 +1,10 @@
 package com.redstonetorch.dongbaekro.emergencyReport.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.redstonetorch.dongbaekro.emergencyReport.entity.EmergencyReport;
 
+@RepositoryRestResource(exported = false)
 public interface EmergencyReportRepository extends JpaRepository<EmergencyReport, Long> {
 }
