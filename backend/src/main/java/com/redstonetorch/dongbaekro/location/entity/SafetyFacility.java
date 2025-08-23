b39291f109fdbb1961fab10e30fb1f530486bd4f
NexTR_Plue <p.plue1881@gmail.com>
2025-08-23 17:47:59 +0900

    fix: 안전시설 entity 행정동 응답 기준으로 column 변경

--- backend/src/main/java/com/redstonetorch/dongbaekro/location/entity/SafetyFacility.java
+++ backend/src/main/java/com/redstonetorch/dongbaekro/location/entity/SafetyFacility.java
@@ -50,18 +50,11 @@ public class SafetyFacility {
 	private String region_3depth_name;
 
 	@Column(nullable = false)
-	private String mountain_yn;
-
-	@Column(nullable = false)
-	private String main_address_no;
-
-	@Column(nullable = false)
-	private String sub_address_no;
+	private String code;
 
 	@Builder
 	public SafetyFacility(SafetyFacilityType type, String name, Double latitude, Double longitude, String address_name,
-		String region_1depth_name, String region_2depth_name, String region_3depth_name, String mountain_yn,
-		String main_address_no, String sub_address_no) {
+		String region_1depth_name, String region_2depth_name, String region_3depth_name, String code) {
 		this.type = type;
 		this.name = name;
 		this.latitude = latitude;
@@ -70,8 +63,6 @@ public class SafetyFacility {
 		this.region_1depth_name = region_1depth_name;
 		this.region_2depth_name = region_2depth_name;
 		this.region_3depth_name = region_3depth_name;
-		this.mountain_yn = mountain_yn;
-		this.main_address_no = main_address_no;
-		this.sub_address_no = sub_address_no;
+		this.code = code;
 	}
 }
